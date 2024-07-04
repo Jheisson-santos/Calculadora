52e6c17c4fdca9c2b57217744f559f8c54295f52
Jheisson-santos <jheisson.g.santos@gmail.com>
2024-07-03 21:03:42 -0300

    adição do laço while

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,9 +1,10 @@
-const prompt = require('prompt-sync')({singit: true})
+const prompt = require('prompt-sync')({sigint: true})
 var soma = require('./adicao')
 var diminuir = require('./subtra')
 var divid = require('./div')
 var vezes = require('./multip')
 var porcentagem = require('./porcent')
+while(true){
 console.log(`
          ________________________
         |                        |
@@ -17,24 +18,36 @@ console.log(`
 `);
 let inp = +prompt('')
 if(!isNaN(inp)){
-if(inp == 5){
+
+    if(inp == 5){
     let num1 = +prompt('Digite a porcentagem: ')
     let num2 = +prompt('Digite o numero: ')
     porcentagem(num1, num2)
+    } else {
+       while(true){
+        let num1 = +prompt('Digite o primeiro numero: ')
+        let num2 = +prompt('Digite o segundo numero: ')
+            if((!isNaN(num1, num2))){
+                if(inp == 1){
+                    console.log(soma(num1, num2))
+                    process.exit()
+                }else if(inp == 2){
+                    console.log(diminuir(num1, num2));
+                    process.exit()
+                }else if(inp == 3){
+                    
+                    if(num2 != 0){
+                        console.log(divid(num1, num2));
+                        process.exit()
+                    } else {console.log('Essa operação não é valida, não use o 0 como divisor');}
+                }else if(inp == 4){
+                    console.log(vezes(num1, num2));
+                    process.exit()
+                }else{console.log('Entrada invalida, digite o numero novamente');}
+            } else {console.log('Entrada invalida');}
+        }
+    }
 } else {
-let num1 = +prompt('Digite o primeiro numero: ')
-let num2 = +prompt('Digite o segundo numero: ')
-if(!isNaN(num1, num2)){
-if(inp == 1){
-console.log(soma(num1, num2))
-}else if(inp == 2){
-console.log(diminuir(num1, num2));
-}else if(inp == 3){
-if(num2 != 0){
-console.log(divid(num1, num2));
-} else {console.log('Essa operação não é valida');}
-}else if(inp == 4){
-console.log(vezes(num1, num2));
-}else{console.log('Entrada invalida, digite o numero novamente');}
-} else {console.log('Entrada invalida');}}
-} else {console.log('Entrada invalido');}
+    console.log('Entrada invalida, digite novamente');
+}
+}
